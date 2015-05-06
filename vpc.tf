ad76238fa39d63b047e35ede619a3be740eefdc0
Mattias Gees <mattias.gees@gmail.com>
2015-05-06 22:40:28 +0200

    Fix subnet_id fault in route table association

--- vpc.tf
+++ vpc.tf
@@ -50,6 +50,6 @@ resource "aws_route_table" "main" {
 # Associate route table to subnets
 resource "aws_route_table_association" "main" {
   count = "${var.amount_subnets}"
-  subnet_id = "${element(split(",", var.subnets_cidr_block), count.index)}"
+  subnet_id = "${element(aws_subnet.main.*.id, count.index)}"
   route_table_id = "${aws_route_table.main.id}"
 }
